8593db1ab7fb5e21bcd736b66ced0bb6cac0dc15
Callia Tang <weitang@ucdavis.edu>
2023-11-06 13:20:23 -0800

    studio 2 html and css

---
+++ studio2/script.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+    console.log('tis javascript my luv');
+
+    const container = document.querySelector('#container');
+    const hotSpots = document.querySelectorAll('#container div');
+    const theImg = document.querySelector('div img');
+
+    hotSpots.forEach(function(eachSpot) {
+        eachSpot.addEventListener('mouseover', zoomPhoto);
+        eachSpot.addEventListener('mouseout', function () {
+            theImg.className='start';
+        })
+    });
+
+    function zoomPhoto(e) {
+        const thisCorner = e.target.id;
+        console.log(thisCorner);
+
+        switch(thisCorner){
+            case 'topleft': theImg.className = 'topleft'; break;
+            case 'topright': theImg.className = 'topright'; break;
+            case 'bottomleft': theImg.className = 'bottomleft'; break;
+            case 'bottomright': theImg.className = 'bottomright'; break;
+            case 'center': theImg.className = 'center'; break;
+        }
+    }
+    
+})();
